0f6a2b3e221f1260d89500add90b49836c02e97d
lizband <lizband@KBM-5100>
2025-04-24 00:41:58 -0400

    Backup

---
+++ RivenFramework-Unity/Assets/RivenFramework-Revamp/PawnSystem/OLD Example FPS/FPS_StatsData.cs
@@ -0,0 +1,97 @@
+//===================== (Neverway 2024) Written by Liz M. =====================
+//
+// Purpose:
+// Notes:
+//
+//=============================================================================
+
+using System.Collections.Generic;
+using UnityEngine;
+/*
+namespace Framework.PawnManagement
+{
+    
+public class FPS_Stats : PawnStats
+{
+    [Header("Personality")] 
+    public float baseHealth;
+    public float courage;
+    public float curiosity;
+    public float lookRange;
+    public float listenRange;
+    public float comfortableAllyDistance;
+    public string team;
+    public List<string> hostileTeams;
+    public List<string> allyTeams;
+        
+    [Header("Movement Speeds")]
+    public float movementSpeed;
+    public float airMovementMultiplier;
+    public float crouchMovementMultiplier;
+    [Header("Movement Acceleration")]
+    public float groundAccelerationRate;
+    public float airAccelerationRate;
+    public float slopeAccelerationRate;
+    
+    [Header("Movement Drag")]
+    public float groundDrag;
+    public float airDrag;
+    public float slopeDrag;
+
+    [Header("Ground Detection & Jumping")] 
+    public float slopeCheckDistance;
+    public LayerMask groundMask;
+    public float groundCheckRadius;
+    public Vector3 groundCheckOffset;
+    public float jumpForce;
+
+    [Header("Head Detection & Crouching")]
+    [Tooltip("The radius of the sphere-cast to check if the head is clear, (this value should be smaller than the radius of the body collider to avoid getting false-positives when a pawn is up against a wall)")]
+    public float headCheckRadius;
+    [Tooltip("The offset from the pawn's origin to start the sphere-cast to check if the head is clear")]
+    public Vector3 headCheckOffset;
+    [Tooltip("The distance of the sphere-cast to check if the head is clear, (This value should normally match, or be greater than, the crouchDistance)")]
+    public float headCheckDistance;
+    [Tooltip("The amount of height to add/subtract from the collider's height when uncrouching/crouching")]
+    public float crouchDistance;
+    public Vector3 crouchColliderOffset;
+
+    /*
+    public FPS_StatsData(string arg)
+    {
+        baseHealth = 100f;
+        courage = 0;
+        curiosity = 0;
+        lookRange = 20f;
+        listenRange = 20f;
+        comfortableAllyDistance = 5f;
+        team = "";
+        hostileTeams = null;
+        allyTeams = null;
+        
+        movementSpeed = 10;
+        airMovementMultiplier = 0.25f;
+        crouchMovementMultiplier = 0.25f;
+        
+        groundAccelerationRate = 0.1f;
+        airAccelerationRate = 0.2f;
+        slopeAccelerationRate = 0.25f;
+    
+        groundDrag = 5;
+        airDrag = 0;
+        slopeDrag = 5;
+        
+        slopeCheckDistance = 0.2f;
+        groundMask = 0;
+        groundCheckRadius = 0.25f;
+        groundCheckOffset = new Vector3();
+        jumpForce = 10;
+
+        headCheckRadius = 0.25f;
+        headCheckOffset = new Vector3(0, 1.75f, 0);
+        headCheckDistance = 0.5f;
+        crouchDistance = 0.5f;
+        crouchColliderOffset = new Vector3(0, 0.25f, 0);
+    }*//*
+}
+}*/
