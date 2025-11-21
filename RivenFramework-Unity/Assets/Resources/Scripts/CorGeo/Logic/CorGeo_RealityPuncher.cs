a7e9c4efe5c1cf807c8aaf7a0b31b84523d760b9
lizband <lizband.dev@gmail.com>
2025-11-21 15:15:09 -0500

    Updated dev model, Working on intro stuffs

--- RivenFramework-Unity/Assets/Resources/Scripts/CorGeo/Logic/CorGeo_RealityPuncher.cs
+++ RivenFramework-Unity/Assets/Resources/Scripts/CorGeo/Logic/CorGeo_RealityPuncher.cs
@@ -8,13 +8,16 @@
 //====================================================================================================================//
 
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A puzzle element that creates teleportation bubbles
+/// </summary>
 public class CorGeo_RealityPuncher : MonoBehaviour
 {
     #region========================================( Variables )======================================================//
     /*-----[ Inspector Variables ]------------------------------------------------------------------------------------*/
+    [Tooltip("The other reality puncher that this one will swap locations with")]
     public CorGeo_RealityPuncher linkedPuncher;
 
 
@@ -25,7 +28,10 @@ public class CorGeo_RealityPuncher : MonoBehaviour
 
 
     /*-----[ Reference Variables ]------------------------------------------------------------------------------------*/
+    [Tooltip("Reference to the sphere trigger volume, used to get which actors are in the teleport bubble")]
     public Volume bubbleVolume;
+    [Tooltip("Reference to this puncher's animator, used for easy access to sync animations between linked punchers")]
+    public Animator animator;
 
 
     #endregion
@@ -36,47 +42,72 @@ public class CorGeo_RealityPuncher : MonoBehaviour
 
 
     /*-----[ Internal Functions ]-------------------------------------------------------------------------------------*/
-
-
-    /*-----[ External Functions ]-------------------------------------------------------------------------------------*/
-    public void Swap()
-    {
-        StopAllCoroutines();
-        linkedPuncher.GetComponent<Animator>().Play("On");
-        StartCoroutine(CoSwap());
-    }
-    
-    public IEnumerator CoSwap()
+    /// <summary>
+    /// Wait for correct point in the animation before swapping
+    /// </summary>
+    private IEnumerator CoSwap()
     {
         yield return new WaitForSeconds(4);
-        var offset = linkedPuncher.transform.position-this.transform.position;
+        SwapBubbledActors();
+        SwapBubbledIslands();
+    }
+    
+    /// <summary>
+    /// Switch the level geometry with the linked puncher's
+    /// </summary>
+    private void SwapBubbledIslands()
+    {
+        // Swap bubble islands
+        // ReSharper disable once SwapViaDeconstruction
+        var originalPosition = gameObject.transform.position;
+        var originalParent = transform.parent;
+        var originalPlaybackTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        gameObject.transform.position = linkedPuncher.transform.position;
+        gameObject.transform.parent = linkedPuncher.transform.parent;
+        linkedPuncher.transform.position = originalPosition;
+        linkedPuncher.transform.parent = originalParent;
         
+        // Link the animation playback times so that both islands being swapped are seamless
+        linkedPuncher.animator.Play("On", 0, originalPlaybackTime);
+    }
+
+    /// <summary>
+    /// Switch the actors in the teleport bubble with the linked puncher's
+    /// </summary>
+    private void SwapBubbledActors()
+    {
         // Move current bubble actors to other bubble
+        var originalOffset = linkedPuncher.transform.position-this.transform.position;
         foreach (var pawn in bubbleVolume.pawnsInTrigger)
         {
-            pawn.transform.position += offset;
+            pawn.transform.position += originalOffset;
         }
         foreach (var prop in bubbleVolume.propsInTrigger)
         {
-            prop.transform.position += offset;
+            prop.transform.position += originalOffset;
         }
         
         // Move other bubble actors to current bubble
         foreach (var pawn in linkedPuncher.bubbleVolume.pawnsInTrigger)
         {
-            pawn.transform.position -= offset;
+            pawn.transform.position -= originalOffset;
         }
         foreach (var prop in linkedPuncher.bubbleVolume.propsInTrigger)
         {
-            prop.transform.position -= offset;
+            prop.transform.position -= originalOffset;
         }
-        
-        
-        // Swap bubble islands
-        // ReSharper disable once SwapViaDeconstruction
-        var currentPosition = gameObject.transform.position;
-        gameObject.transform.position = linkedPuncher.transform.position;
-        linkedPuncher.transform.position = currentPosition;
+    }
+
+
+    /*-----[ External Functions ]-------------------------------------------------------------------------------------*/
+    /// <summary>
+    /// Switch this puncher with the linked one
+    /// </summary>
+    public void Swap()
+    {
+        animator.Play("On");
+        StopAllCoroutines();
+        StartCoroutine(CoSwap());
     }
 
 
