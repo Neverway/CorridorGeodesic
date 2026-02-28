2678e3f8500d0c5661971dbd2afced2cd649256b
Lizband <lizband.dev@gmail.com>
2026-02-28 18:46:36 -0500

    backup of wip stuff

---
+++ RivenFramework-Unity/Assets/Resources/Scripts/CorGeo/Logic/Logic_BellTower.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic_BellTower : MonoBehaviour
+{
+    public TowerType towerType;
+    public enum TowerType
+    {
+        unstable,
+        stable,
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
