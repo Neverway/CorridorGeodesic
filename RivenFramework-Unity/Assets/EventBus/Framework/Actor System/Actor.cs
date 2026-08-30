e285f1551e916dcd2e8533f1b43b9335ffe9a144
Lizband <lizband.dev@gmail.com>
2026-08-29 20:43:54 -0400

    Backup before I make a mistake (I have already made a mistake by needing to make this commit)

--- RivenFramework-Unity/Assets/EventBus/Framework/Actor System/Actor.cs
+++ RivenFramework-Unity/Assets/EventBus/Framework/Actor System/Actor.cs
@@ -1,58 +1,174 @@
+//==========================================( Neverway 2026 )=========================================================//
+// Author
+// Liz M.
+//
+// Contributors
+//  Errynei, Connorses, Soulex
+//
+//====================================================================================================================//
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
-/// <summary>
-/// Acts as an identifier for an object, assigning it an id, what groups it's a part of, and a human readable display name
-/// </summary>
-public class BActor : MonoBehaviour
+namespace NewRivenFramework.Core
 {
-    public string id;
-    public string displayName;
-    public List<ActorGroup> groups;
-
-    public bool IsInAnyOfGroups(List<ActorGroup> _groups)
+    /// <summary>
+    /// Acts as the base identifier for an asset that can be placed in a map
+    /// Assigning it an id, what groups it's a part of, and a human-readable display name
+    /// All placeable assets must contain this component on their root
+    /// </summary>
+    public class Actor : MonoBehaviour
     {
-        return groups.Intersect(_groups).Any();
-    }
-}
+        #region========================================( Variables )====================================================== //
 
-[Serializable]
-public abstract class ActorFilter
-{
-    public abstract bool PassesFilter(BActor _actor);
-}
+        /*-----[ Inspector Variables ]------------------------------------------------------------------------------------*/
+        [Header("Actor Data")]
+        [Tooltip("This ID is how this actor is identified, saved, and loaded from map files")]
+        public string id;
+        [Tooltip("This is how this actor is listed in things like an asset browser, or in game like in an inventory")]
+        public string displayName;
+        [Tooltip("This is a unique id to this individual actor, it's used to differentiate between instances of the same type of object in map")]
+        public string uniqueId;
+        [Tooltip("This is what groups this actor is associated with, it's used to filter between different kinds of objects when handling things like logic volumes")]
+        public List<ActorGroup> groups;
 
-[Serializable]
-public class ActorFilter_IsNamed : ActorFilter
-{
-    public string name;
+
+        /*-----[ External Variables ]-------------------------------------------------------------------------------------*/
+
+
+        /*-----[ Internal Variables ]-------------------------------------------------------------------------------------*/
+        [ContextMenu("Generate ID & Name")]
+        private void GenerateIDAndName()
+        {
+            GenerateID();
+            GenerateDisplayName();
+        }
     
-    public override bool PassesFilter(BActor _actor)
+        [ContextMenu("Generate ID")]
+        private void GenerateID()
+        {
+            // Generate a UUID
+            id = gameObject.name;
+        }
+    
+        [ContextMenu("Generate Display Name")]
+        private void GenerateDisplayName()
+        {
+            displayName = Regex.Replace(gameObject.name, "([a-z])([A-Z])", "$1 $2");
+            displayName = Regex.Replace(displayName, "^[^_]*_", "");
+        }
+    
+        [ContextMenu("Generate UUID")]
+        private void GenerateUID()
+        {
+            // Generate a UUID
+            uniqueId = Guid.NewGuid().ToString();
+
+            // Check if it's taken
+            if (CheckUUID() is false)
+            {
+                Debug.Log("UUID was already taken");
+                GenerateUID();
+            }
+        }
+
+        [ContextMenu("Check UUID")]
+        private bool CheckUUID()
+        {
+            foreach (var actor in FindObjectsOfType<Actor>())
+            {
+                if (actor == this) continue;
+                if (actor.uniqueId == uniqueId)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        /*-----[ Reference Variables ]------------------------------------------------------------------------------------*/
+
+
+
+        #endregion
+
+
+        #region=======================================( Functions )======================================================= //
+
+        /*-----[ Mono Functions ]-----------------------------------------------------------------------------------------*/
+
+        
+        /*-----[ Internal Functions ]-------------------------------------------------------------------------------------*/
+
+
+        /*-----[ External Functions ]-------------------------------------------------------------------------------------*/
+        public bool IsInAnyOfGroups(List<ActorGroup> _groups)
+        {
+            return groups.Intersect(_groups).Any();
+        }
+
+        [ContextMenu("Check UUID")]
+        public List<Actor> GetConflictingDuplicateInstancesOfActor()
+        {
+            List<Actor> conflictingActors = new List<Actor>();
+            foreach (var actor in FindObjectsOfType<Actor>())
+            {
+                if (actor.uniqueId == uniqueId)
+                {
+                    conflictingActors.Add(this);
+                }
+            }
+
+            return conflictingActors;
+        }
+
+
+        #endregion
+    }
+    
+    [Serializable]
+    public abstract class ActorFilter
     {
-        return _actor.displayName == name;
+        public abstract bool PassesFilter(Actor _actor);
+    }
+
+    [Serializable]
+    public class ActorFilter_IsNamed : ActorFilter
+    {
+        public string name;
+    
+        public override bool PassesFilter(Actor _actor)
+        {
+            return _actor.displayName == name;
+        }
+    }
+
+    [Serializable]
+    public class ActorFilter_IsID : ActorFilter
+    {
+        public string id;
+
+        public override bool PassesFilter(Actor _actor)
+        {
+            return _actor.id == id;
+        }
+    }
+
+    [Serializable]
+    public class ActorFilter_IsInAnyGroup : ActorFilter
+    {
+        public List<ActorGroup> groups;
+
+        public override bool PassesFilter(Actor _actor)
+        {
+            return _actor.IsInAnyOfGroups(groups);
+        }
     }
 }
 
-[Serializable]
-public class ActorFilter_IsID : ActorFilter
-{
-    public string id;
-
-    public override bool PassesFilter(BActor _actor)
-    {
-        return _actor.id == id;
-    }
-}
-
-[Serializable]
-public class ActorFilter_IsInAnyGroup : ActorFilter
-{
-    public List<ActorGroup> groups;
-
-    public override bool PassesFilter(BActor _actor)
-    {
-        return _actor.IsInAnyOfGroups(groups);
-    }
-}
---
+++ RivenFramework-Unity/Assets/EventBus/Framework/Events/GameInstance_EventDistributor_Health.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using RivenFramework.EventBus;
+using UnityEngine;
+
+namespace NewRivenFramework.EventBus
+{
+    /// <summary>
+    /// Distributes health events to the appropriate health receiver
+    /// </summary>
+    public class GameInstance_EventDistributor_Health : MonoBehaviour
+    {
+        public List<HealthReceiver> healthReceivers = new List<HealthReceiver>();
+        private void OnEnable()
+        {
+            EventBus<Event_ModifyHealth>.Subscribe(DistributeModifyHealthEventToReceivers);
+        }
+        
+        private void OnDisable()
+        {
+            EventBus<Event_ModifyHealth>.Unsubscribe(DistributeModifyHealthEventToReceivers);
+        }
+
+        private void DistributeModifyHealthEventToReceivers(Event_ModifyHealth eventModifyHealth)
+        {
+            Debug.Log("Event Called");
+            Debug.Log($"count {healthReceivers.Count}");
+            foreach (var healthReceiver in healthReceivers)
+            {
+                Debug.Log($"{healthReceiver.gameObject.name} {eventModifyHealth.Target} {healthReceiver.target}");
+                if (eventModifyHealth.Target == healthReceiver.target)
+                {
+                    Debug.Log($"{healthReceiver.gameObject.name} is valid");
+                    healthReceiver.ModifyHealth(eventModifyHealth);
+                }
+            }
+        }
+    }
+
+    public class Event_ModifyHealth : BaseEvent
+    {
+        public override bool LogEventCalls() => true;
+
+        public int DamageAmount;
+        [Polymorphic, SerializeReference] public List<ActorFilter> AffectedActorsFilters;
+        public Actor Target;
+        public Actor Source;
+        
+        public override string GetEventDescription()
+        {
+            return $"DamageAmount:{DamageAmount}, AffectedActorsFilters:{AffectedActorsFilters}, Target:{Target}, Source:{Source}";
+        }
+
+    }
+
+    public class Event_Death : BaseEvent
+    {
+        
+        public Actor Target;
+        public Event_ModifyHealth CauseOfDeath;
+    }
+}
